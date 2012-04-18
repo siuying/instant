3f0ab137d332a651213adf1b96abe1fd930a6e9f
Francis Chong <francis@ignition.hk>
2012-04-18 23:47:07 +0800

    properly return stack trace for debug

--- lib/instant/runner.rb
+++ lib/instant/runner.rb
@@ -14,13 +14,20 @@ module Instant
         return_value = context.instance_eval(@processed)
         context.close
         {:status => :ok, :result => context.to_s, :return_value => return_value}
+      rescue SyntaxError => e
+        {:status => :error, :cause => :syntax_error, :message => format_error(e), :result => context.to_s }
       rescue Racc::ParseError => e
-        {:status => :error, :cause => :parse_error, :message => "Parse Error: " + e.message, :result => context.to_s }
+        {:status => :error, :cause => :parse_error, :message => format_error(e), :result => context.to_s }
       rescue Instant::LoopTooDeepError => e
         {:status => :error, :cause => :loop_too_deep, :message => "Loop too deep", :result => context.to_s }
       rescue StandardError => e
-        {:status => :error, :cause => :unknown, :message => e.message, :result => context.to_s }        
+        {:status => :error, :cause => :unknown, :message => format_error(e), :result => context.to_s }        
       end
+    end
+    
+    private
+    def format_error(e)
+      "#{e.message}<br><br>#{ e.backtrace[0..10].join('<br>')}"
     end
   end
 end
